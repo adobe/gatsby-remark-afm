3b89d202588730d1369c98ffb44dfbb4622dafbe
Simon MacDonald <simon.macdonald@gmail.com>
2020-01-13 23:33:47 -0500

    🔧  Refactor admonitions to 🔥code

--- src/Admonitions.js
+++ src/Admonitions.js
@@ -5,54 +5,37 @@ function admonitions(markdownAST) {
   visit(markdownAST, "blockquote", node => {
     console.log(node);
     let children = node.children;
+    let html = null;
     if (
       children.length > 0 &&
       children[0].type === "paragraph" &&
       children[0].children.length > 0 &&
-      children[0].children[0].type === "linkReference" &&
-      children[0].children[0].label === "!NOTE"
+      children[0].children[0].type === "linkReference"
     ) {
-      console.log("found a note");
-      const html = `<Alert header="Note" variant="info">${children[1].children[0].value}</Alert>`;
-      node.type = "html";
-      node.children = undefined;
-      node.value = html;
-    } else if (
-      children.length > 0 &&
-      children[0].type === "paragraph" &&
-      children[0].children.length > 0 &&
-      children[0].children[0].type === "linkReference" &&
-      children[0].children[0].label === "!TIP"
-    ) {
-      console.log("found a tip");
-      const html = `<Alert header="Tip" variant="help">${children[1].children[0].value}</Alert>`;
-      node.type = "html";
-      node.children = undefined;
-      node.value = html;
-    } else if (
-      children.length > 0 &&
-      children[0].type === "paragraph" &&
-      children[0].children.length > 0 &&
-      children[0].children[0].type === "linkReference" &&
-      children[0].children[0].label === "!CAUTION"
-    ) {
-      console.log("found a caution");
-      const html = `<Alert header="Caution" variant="error">${children[1].children[0].value}</Alert>`;
-      node.type = "html";
-      node.children = undefined;
-      node.value = html;
-    } else if (
-      children.length > 0 &&
-      children[0].type === "paragraph" &&
-      children[0].children.length > 0 &&
-      children[0].children[0].type === "linkReference" &&
-      children[0].children[0].label === "!WARNING"
-    ) {
-      console.log("found a tip");
-      const html = `<Alert header="Warning" variant="warning">${children[1].children[0].value}</Alert>`;
-      node.type = "html";
-      node.children = undefined;
-      node.value = html;
+      switch (children[0].children[0].label) {
+        case "!NOTE":
+          console.log("found a note");
+          html = `<Alert header="Note" variant="info">${children[1].children[0].value}</Alert>`;
+          break;
+        case "!TIP":
+          console.log("found a tip");
+          html = `<Alert header="Tip" variant="help">${children[1].children[0].value}</Alert>`;
+          break;
+        case "!CAUTION":
+          console.log("found a caution");
+          html = `<Alert header="Caution" variant="error">${children[1].children[0].value}</Alert>`;
+          break;
+        case "!WARNING":
+          console.log("found a tip");
+          html = `<Alert header="Warning" variant="warning">${children[1].children[0].value}</Alert>`;
+          break;
+      }
+
+      if (html) {
+        node.type = "html";
+        node.children = undefined;
+        node.value = html;
+      }
     }
   });
 }
