3e3ed1a4ffe67c0c0f0a2545c2c37a76b7435096
Simon MacDonald <simon.macdonald@gmail.com>
2020-04-07 11:15:28 -0400

    💄 Remove console.log's

--- src/Admonitions.js
+++ src/Admonitions.js
@@ -2,8 +2,7 @@ const visit = require("unist-util-visit");
 const toString = require("mdast-util-to-string");
 
 function admonitions(markdownAST) {
-  visit(markdownAST, "blockquote", node => {
-    console.log(node);
+  visit(markdownAST, "blockquote", (node) => {
     let children = node.children;
     let html = null;
     if (
@@ -14,19 +13,15 @@ function admonitions(markdownAST) {
     ) {
       switch (children[0].children[0].label) {
         case "!NOTE":
-          console.log("found a note");
           html = `<Alert header="Note" variant="info">${children[1].children[0].value}</Alert>`;
           break;
         case "!TIP":
-          console.log("found a tip");
           html = `<Alert header="Tip" variant="help">${children[1].children[0].value}</Alert>`;
           break;
         case "!CAUTION":
-          console.log("found a caution");
           html = `<Alert header="Caution" variant="error">${children[1].children[0].value}</Alert>`;
           break;
         case "!WARNING":
-          console.log("found a tip");
           html = `<Alert header="Warning" variant="warning">${children[1].children[0].value}</Alert>`;
           break;
       }
