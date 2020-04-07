061070864794e97e933f8121f11de0fd33a330db
Simon MacDonald <simon.macdonald@gmail.com>
2020-04-07 11:13:57 -0400

    🎉 Re-enabled include relative

--- src/IncludeRelative.js
+++ src/IncludeRelative.js
@@ -3,32 +3,29 @@ const visit = require("unist-util-visit");
 function includeRelative(markdownAST) {
   visit(markdownAST, "paragraph", (node) => {
     if (node.children.length > 0) {
-      for (let i = 0; i < node.children.length; i++) {
-        let text = node.children[i].value;
-        let matches = text.match(/{%\s*include_relative\s*(.*?)\s*%}/g);
-        if (matches && matches.length > 0) {
-          for (let j = 0; j < matches.length; j++) {
-            let filename = matches[j]
-              .replace("{%", "")
-              .replace("%}", "")
-              .replace("include_relative", "")
-              .trim();
-            text = text.replace(
-              matches[j],
-              `<IncludeMarkdown file="${filename}"/ >`
-            );
+      for (let child of node.children) {
+        let text = child.value;
+        if (text) {
+          const matches = text.match(/{%\s*include_relative\s*(.*?)\s*%}/g);
+          if (matches && matches.length > 0) {
+            for (const match of matches) {
+              let filename = match
+                .replace("{%", "")
+                .replace("%}", "")
+                .replace("include_relative", "")
+                .trim();
+              text = text.replace(
+                match,
+                `<IncludeMarkdown file="${filename}"/ >`
+              );
+            }
+            child.type = `html`;
+            child.value = `<div>${text}</div>`;
           }
-          node.children[i].type = `html`;
-          node.children[i].value = `<div>${text}</div>`;
-          // find everything between the {% %} /{%\s*include_relative\s*(.*?)\s*%}/g
-          // extract file name
-          // add tag to load in html from other file
-          // wrap everything in a div in case of other text
         }
       }
     }
   });
-  // console.log(JSON.stringify(markdownAST));
 }
 
 module.exports = includeRelative;
