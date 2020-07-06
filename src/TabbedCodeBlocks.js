6b9e35c1862d1a345511bd8e6c3c06355f19d773
Simon MacDonald <simon.macdonald@gmail.com>
2020-07-06 16:55:58 -0400

    👕 Fix linting errors

--- src/TabbedCodeBlocks.js
+++ src/TabbedCodeBlocks.js
@@ -10,8 +10,6 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 const visit = require("unist-util-visit");
-const remove = require("unist-util-remove");
-const toString = require("mdast-util-to-string");
 
 function tabbedCodeBlocks(markdownAST) {
   // console.log(JSON.stringify(markdownAST));
